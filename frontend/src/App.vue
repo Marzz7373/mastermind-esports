fc86e2da879fff0338759394832a01c5deec3511
Marzz7373 <mnmarzuqi@gmail.com>
2025-07-21 22:48:03 +0800

    App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './pages/Home.vue'
+import Game from './pages/Game.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/game/:slug', component: Game },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+</script>
